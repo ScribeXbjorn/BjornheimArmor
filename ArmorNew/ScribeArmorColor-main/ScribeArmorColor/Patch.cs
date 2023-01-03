66f0f3da71d085d5154c5e0a23f09c586276b08d
ScribeXbjorn <121883528+ScribeXbjorn@users.noreply.github.com>
2023-01-03 18:04:38 +0200

    Add files via upload

---
+++ ArmorNew/ScribeArmorColor-main/ScribeArmorColor/Patch.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using static ScribeArmorColor.Plugin;
+
+namespace ScribeArmorColor
+{
+    [HarmonyPatch]
+    internal class Patch
+    {
+        [HarmonyPatch(typeof(ObjectDB), nameof(ObjectDB.Awake)), HarmonyPostfix]
+        public static void ObjectDBAwakePatchPostfix()
+        {
+            GameObject helmetNOrig = ObjectDB.instance.GetItemPrefab("ByzantiteHelmet");
+            Renderer renderer = helmetNOrig.transform.Find("attach").Find("Helmet.003").GetComponent<Renderer>();
+            renderer.enabled = false;
+            helmetNOrig.GetComponent<ItemDrop>().m_itemData.m_shared.m_icons[0] = _self.helmetNIcon;
+
+            /*Material material = renderer.material;
+            material.color = new(46, 106, 120, 255);
+            material.DisableKeyword("_Emission");*/
+
+            GameObject armorNOrig = ObjectDB.instance.GetItemPrefab("ByzantiteChest");
+            armorNOrig.transform.Find("attach_skin").Find("Torso.014").GetComponent<Renderer>().material = _self.chestMaterial;
+            armorNOrig.GetComponent<ItemDrop>().m_itemData.m_shared.m_icons[0] = _self.armorNIcon;
+
+            GameObject armorOOrig = ObjectDB.instance.GetItemPrefab("LokiSerpentsChest");
+            armorOOrig.GetComponent<ItemDrop>().m_itemData.m_shared.m_icons[0] = _self.armorOIcon;
+
+            GameObject legsOOrig = ObjectDB.instance.GetItemPrefab("LokiSerpentsLegs");
+            legsOOrig.GetComponent<ItemDrop>().m_itemData.m_shared.m_icons[0] = _self.legsOIcon;
+
+            GameObject helmetOOrig = ObjectDB.instance.GetItemPrefab("LokiSerpentsHelmet");
+            helmetOOrig.GetComponent<ItemDrop>().m_itemData.m_shared.m_icons[0] = _self.helmetOIcon;
+        }
+    }
+}
---
+++ ArmorNew/ScribeArmorColor-main/ScribeArmorColor/Plugin.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx;
+using HarmonyLib;
+using ItemManager;
+using UnityEngine;
+
+namespace ScribeArmorColor
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    [BepInDependency("DarkArmor", BepInDependency.DependencyFlags.HardDependency)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal const string ModName = "ScribeArmorColor", ModVersion = "0.0.1", ModGUID = "com.Frogger." + ModName;
+        private static readonly Harmony harmony = new(ModGUID);
+        internal static Plugin _self;
+        private AssetBundle assetBundle;
+        internal Material chestMaterial;
+        internal Sprite helmetNIcon;
+        internal Sprite armorNIcon;
+        internal Sprite armorOIcon;
+        internal Sprite helmetOIcon;
+        internal Sprite legsOIcon;
+
+        private void Awake()
+        {
+            _self = this;
+
+            assetBundle = PrefabManager.RegisterAssetBundle("armor");
+            chestMaterial = assetBundle.LoadAsset<Material>("ChestMaterial");
+            helmetNIcon = assetBundle.LoadAsset<Sprite>("HelmetIcon");
+            armorNIcon = assetBundle.LoadAsset<Sprite>("ArmorIcon");
+
+            armorOIcon = assetBundle.LoadAsset<Sprite>("OdinsArmor");
+            helmetOIcon = assetBundle.LoadAsset<Sprite>("OdinsHelmet");
+            legsOIcon = assetBundle.LoadAsset<Sprite>("OdinsLegs");
+
+            harmony.PatchAll();
+        }
+
+        internal void Debug(string msg)
+        {
+            Logger.LogInfo(msg);
+        }
+    }
+}
